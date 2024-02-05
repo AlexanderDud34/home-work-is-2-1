d37fc802c37d9858b3b28effa8dea5c095fe9574
AlexanderDud34 <adudkin45@gmail.com>
2024-02-05 12:41:24 +0300

    Выполнили работу на массивы

---
+++ Task_3/main.js
@@ -0,0 +1,30 @@
+// const students = ["Андрей", "Иван", "Стас"];
+// students [50] = "Индекс 50"; 
+// console.log(students);
+
+
+// let college = [  ["Андрей", "Иван", "Стас"], [205, 111, 304]  ];
+// let kab = college[1][1] = 112
+// console.log(college);
+
+
+
+
+let number = [ 1,2,3,4 ];
+
+const coutLenght = 5;
+
+if ( number.length === coutLenght ) {
+    console.log("ok");
+} else if ( number.length < coutLenght ) {
+    let result = coutLenght - number.length;
+    console.log(`Длина вашего массива: ${number.length}. Длину массива нужно увеличить до ${coutLenght}. Прибавьте к массиву значение ${result}`);
+}
+
+
+
+
+
+
+
+
