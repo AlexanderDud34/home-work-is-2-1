7282267816b3860c33ea879393b8e63c2b002696
AlexanderDud34 <adudkin45@gmail.com>
2024-02-05 16:02:45 +0300

    Редактировал классную работу

--- Task_3/main.js
+++ Task_3/main.js
@@ -28,3 +28,37 @@ if ( number.length === coutLenght ) {
 
 
 
+
+let person = {
+    name: "Tom",
+    age: 18,
+    hobbies: ["Кушать", "Спать", "Просмотр кино", "Читать книги"],
+    contacts: {
+        phone: [88005553255, 88905553255],
+        email: ["tom@example.com", "tom2@example.com"],
+        social: "www.github.com"
+    }
+}
+
+console.log(`Имя - ${person["name"]}. Возраст - ${person["age"]}`);
+console.log(person["hobbies"] [2]);
+console.log(person["contacts"]["email"][1]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
